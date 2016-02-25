83960bbbd8fffb2284528a1af5db5e2cc0831fd0
Matt <macmanes@gmail.com>
2016-02-25 07:35:28 -0500

    add seqtk

--- install.sh
+++ install.sh
@@ -6,4 +6,7 @@ make -j4
 cd ..
 ./configure --with-boost=/home/macmanes/boost_1_60_0/build/
 make -j4
-echo PATH=$PATH:$(pwd) >> ~/.profile
+git clone https://github.com/lh3/seqtk.git
+cd seqtk
+make -j4
+echo PATH=$PATH:$(pwd):$(pwd)/seqtk >> ~/.profile
