f72360060c7b7e5f97cba099339bf17f729aa7d5
Matt <macmanes@gmail.com>
2016-02-25 12:32:33 -0500

    whoopsie

--- install.sh
+++ install.sh
@@ -13,5 +13,5 @@ make -j4
 cd ../../
 echo PATH=$PATH:$(pwd):$(pwd)/plugins/seqtk >> ~/.profile
 echo $(pwd)
-BinPacker -d -q -s fq -p pair -l ./sample_test/reads.left.fq -r ./sample_test/reads.right.fq -m RF -k 25 -g 200 -o test
+$(pwd)/BinPacker -d -q -s fq -p pair -l ./sample_test/reads.left.fq -r ./sample_test/reads.right.fq -m RF -k 25 -g 200 -o test
 rm -fr test
