0f2622f46d0ea3160dc7c68e3fcac49813bfa36f
Matt <macmanes@gmail.com>
2016-02-25 10:59:31 -0500

    update

--- install.sh
+++ install.sh
@@ -10,6 +10,8 @@ cd plugins
 git clone https://github.com/lh3/seqtk.git
 cd seqtk
 make -j4
+cd ../../
 echo PATH=$PATH:$(pwd):$(pwd)/plugins/seqtk >> ~/.profile
+echo $(pwd)
 BinPacker -s fq -p pair -l ./sample_test/reads.left.fq -r ./sample_test/reads.right.fq -m RF -k 25 -g 200 -o test -d
 rm -fr test
