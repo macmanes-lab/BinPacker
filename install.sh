25180d7d6aa63fa299e757b85988eae925350a1c
Matt MacManes <matthew.macmanes@unh.edu>
2016-02-25 08:14:43 -0500

    add test

--- install.sh
+++ install.sh
@@ -11,3 +11,5 @@ git clone https://github.com/lh3/seqtk.git
 cd seqtk
 make -j4
 echo PATH=$PATH:$(pwd):$(pwd)/plugins/seqtk >> ~/.profile
+BinPacker -s fq -p pair -l ./sample_test/reads.left.fq -r ./sample_test/reads.right.fq -m RF -k 25 -g 200 -o test -d
+rm -fr test
