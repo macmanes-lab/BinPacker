b0a24b5e6ae4d3ffbfc6281c9756a0ed30be1925
Matt MacManes <matthew.macmanes@unh.edu>
2016-05-19 14:07:24 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,5 +14,5 @@ cd ../../
 echo PATH=$PATH:$(pwd):$(pwd)/plugins/seqtk >> ~/.profile
 echo LD_LIBRARY_PATH=/home/ubuntu/boost/lib/ >> ~/.profile
 source ~/.profile
-$(pwd)/BinPacker -d -q -s fq -p pair -l ./sample_test/reads.left.fq -r ./sample_test/reads.right.fq -m RF -k 25 -g 200 -o test
+BinPacker -d -q -s fq -p pair -l ./sample_test/reads.left.fq -r ./sample_test/reads.right.fq -m RF -k 25 -g 200 -o test
 rm -fr test
